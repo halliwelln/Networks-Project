0bbd6c0436fed966b166c7a20960bbf7e408e772
halliwelln <nicholas.halliwell@barcelonagse.eu>
2016-06-23 16:24:35 +0200

    push

--- SVM.py
+++ SVM.py
@@ -7,22 +7,25 @@ import json
 import math
 import numpy  as np
 import pandas as pd
+import random
 from sklearn.svm import SVC
 from sklearn.ensemble   import RandomForestClassifier
 from sklearn.metrics    import f1_score
+from sklearn.multiclass import OneVsRestClassifier
 
 # ----------------------------------------------------------------
 # Load Data
 # ----------------------------------------------------------------
 
-os.chdir('/Users/felix/Documents/GSE/Term 3/14D009 Social and Economic Networks/Project Data/')
-
-X = pd.read_csv('mod_matrix.csv')
-Y = pd.read_csv('labels.csv')
+#os.chdir('/Users/felix/Documents/GSE/Term 3/14D009 Social and Economic Networks/Project Data/')
+os.chdir('/Users/Nick/Desktop')
+X = pd.read_csv('mod_matrix.csv', header=None)
+Y = pd.read_csv('labels.csv', header=None)
 
 # ----------------------------------------------------------------
 # Run models
 # ----------------------------------------------------------------
+np.random.seed(123)
 
 N    = X.shape[0]
 ind  = random.sample(range(N), N)
@@ -38,16 +41,19 @@ X_test = X.ix[test_ind,:]
 Y_test = Y.ix[test_ind,:]
 
 # Train multi-label classifier
-classifier = OneVsRestClassifier(SVC(C=1))
+classifier = OneVsRestClassifier(SVC(kernel='linear'))
 classifier.fit(X_training, Y_training)
 
 # Predict multi labels
-predictions = classif.predict(X_test)
+predictions = classifier.predict(X_test)
 
 # Compute micro and macro F1-Score
-micro = f1_score(true_labels,predictions,average='micro')
-macro = f1_score(true_labels,predictions,average='macro')
+micro = f1_score(Y_test,predictions,average='micro')
+macro = f1_score(Y_test,predictions,average='macro')
 
 # ----------------------------------------------------------------
 # END CODE
 # ----------------------------------------------------------------
+
+print micro, "micro"
+print macro, "macro"
